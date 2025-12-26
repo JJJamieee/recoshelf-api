52a4a31047f3eb968520d16bb35341430640e53f
Jamie <jamie20000303@gmail.com>
2025-12-26 16:00:31 +0900

    feature: add validator

--- pkg/app_errors/app_errors.go
+++ pkg/app_errors/app_errors.go
@@ -1,7 +1,11 @@
 package app_errors
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type AppError struct {
@@ -12,6 +16,31 @@ type AppError struct {
 
 func (e *AppError) Error() string {
 	return e.Message
+}
+
+func InvalidRequestBodyError(errs error) *AppError {
+	var errMsg string
+	if validateErr, ok := errs.(validator.ValidationErrors); ok {
+		errMsgs := make([]string, 0)
+		for _, err := range validateErr {
+			errMsgs = append(errMsgs, fmt.Sprintf(
+				"field: %s, value: %s, error: %s",
+				err.Field(),
+				err.Value(),
+				err.Tag(),
+			))
+		}
+
+		errMsg = strings.Join(errMsgs, " | ")
+	} else {
+		errMsg = errs.Error()
+	}
+
+	return &AppError{
+		Status:  http.StatusBadRequest,
+		Code:    "INVALID_REQUEST_BODY",
+		Message: errMsg,
+	}
 }
 
 // Common
