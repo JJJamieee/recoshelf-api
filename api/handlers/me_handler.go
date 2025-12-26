c30521031507666e39f739f12966a32d83417d7b
Jamie <jamie20000303@gmail.com>
2025-12-26 16:06:42 +0900

    feature: add Add User Release API POST /me/releases

--- api/handlers/me_handler.go
+++ api/handlers/me_handler.go
@@ -3,9 +3,11 @@ package handlers
 import (
 	"log/slog"
 
+	"recoshelf-api/api/requests"
 	"recoshelf-api/pkg/app_errors"
 	"recoshelf-api/pkg/services"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -23,5 +25,33 @@ func GetUserReleases(releaseService services.ReleaseService) fiber.Handler {
 			return app_errors.InternalServerError
 		}
 		return c.JSON(releases)
+	}
+}
+
+func CreateUserRelease(releaseService services.ReleaseService, validate *validator.Validate) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		releaseRequest := new(requests.ReleaseRequest)
+		if err := c.BodyParser(releaseRequest); err != nil {
+			return app_errors.InvalidRequestBodyError(err)
+		}
+
+		if errs := validate.Struct(releaseRequest); errs != nil {
+			return app_errors.InvalidRequestBodyError(errs)
+		}
+
+		userID, ok := c.Locals("userID").(int)
+		if !ok {
+			slog.Error("Invalid user ID.", "userID", userID)
+			return app_errors.InvalidUserIDError
+		}
+
+		release := releaseRequest.ToEntity()
+		err := releaseService.CreateUserRelease(int64(userID), release)
+		if err != nil {
+			slog.Error("Create user releases failed.", "msg", err.Error())
+			return app_errors.InternalServerError
+		}
+
+		return c.SendStatus(201)
 	}
 }
--- pkg/repositories/release_repository.go
+++ pkg/repositories/release_repository.go
@@ -1,13 +1,19 @@
 package repositories
 
 import (
+	"errors"
+
 	"recoshelf-api/pkg/entities"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
 
 type ReleaseRepository interface {
-	GetReleasesByUser(userID int) (*[]entities.Release, error)
+	GetReleaseByID(releaseID int64) (*entities.Release, error)
+	GetReleasesByUser(userID int64) (*[]entities.Release, error)
+	CreateRelease(release entities.Release) (int64, error)
+	CreateReleaseUserRelation(userID int64, releaseID int64) error
 }
 
 type releaseRepository struct {
@@ -20,7 +26,18 @@ func NewReleaseRepo(db *sqlx.DB) ReleaseRepository {
 	}
 }
 
-func (r *releaseRepository) GetReleasesByUser(userID int) (*[]entities.Release, error) {
+func (r *releaseRepository) GetReleaseByID(releaseID int64) (*entities.Release, error) {
+	q := `
+		SELECT * FROM releases WHERE id = ?
+	`
+
+	release := entities.Release{}
+	err := r.DB.Get(&release, q, releaseID)
+
+	return &release, err
+}
+
+func (r *releaseRepository) GetReleasesByUser(userID int64) (*[]entities.Release, error) {
 	q := `
 		SELECT releases.* 
 		FROM releases 
@@ -32,4 +49,44 @@ func (r *releaseRepository) GetReleasesByUser(userID int) (*[]entities.Release, 
 	err := r.DB.Select(&releases, q, userID)
 
 	return &releases, err
+}
+
+func (r *releaseRepository) CreateRelease(release entities.Release) (int64, error) {
+	q := `
+		INSERT INTO releases (title, country, genres, release_year, tracklist, images, barcode)
+		VALUES (:title, :country, :genres, :release_year, :tracklist, :images, :barcode)
+	`
+
+	result, err := r.DB.NamedExec(q, release)
+	if err != nil {
+		return 0, err
+	}
+
+	releaseID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return releaseID, nil
+}
+
+func (r *releaseRepository) CreateReleaseUserRelation(userID int64, releaseID int64) error {
+	q := `
+		INSERT INTO releases_users (release_id, user_id) VALUES (:releaseID, :userID)
+	`
+
+	_, err := r.DB.NamedExec(q, map[string]interface{}{
+		"userID":    userID,
+		"releaseID": releaseID,
+	})
+
+	if err != nil {
+		var mysqlErr *mysql.MySQLError
+		// Check if duplicate
+		if errors.As(err, &mysqlErr) && mysqlErr.Number == 1062 {
+			return nil
+		}
+	}
+
+	return err
 }
