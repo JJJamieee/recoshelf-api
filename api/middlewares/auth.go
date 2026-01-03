bb1c016d222f414ccba949c8866d798f2c6f02e7
Jamie <jamie20000303@gmail.com>
2026-01-03 22:21:24 +0900

    feature: update mock auth to use header X-User-Id

--- api/middlewares/auth.go
+++ api/middlewares/auth.go
@@ -1,12 +1,20 @@
 package middlewares
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func Protected(c *fiber.Ctx) error {
 	// TODO replace with JWT auth
-	c.Locals("userID", 1)
+	userID := 1
+	if headerVal := c.Get("X-User-Id"); headerVal != "" {
+		if parsed, err := strconv.Atoi(headerVal); err == nil && parsed > 0 {
+			userID = parsed
+		}
+	}
+	c.Locals("userID", userID)
 
 	return c.Next()
 }
